0b7200c076e17745713f01eb5d17487e5a991ff7
Hridoy Ahmed <hridoyahmed47@gmail.com>
2025-02-26 11:33:48 +0600

    in app page sidebar and map component are created. components are placed properly (from starter folder)

--- src/Pages/AppLayout.jsx
+++ src/Pages/AppLayout.jsx
@@ -1,9 +1,12 @@
-import AppNav from "../Components/AppNav";
+
+import Map from "../Components/Map";
+import Sidebar from "../Components/Sidebar";
+import styles from './AppLayout.module.css'
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>App</p>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
