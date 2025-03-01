4566840787f31bf0a27d5b8d0e22ca2bba452b85
Hridoy Ahmed <hridoyahmed47@gmail.com>
2025-03-01 12:47:59 +0600

    stored data in url with query and made it universally accesible with useSearchParams

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,10 +1,26 @@
-import styles from './Map.module.css'
+import { useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
 function Map() {
-    return (
-        <div className={styles.mapContainer}>
-            MAP
-        </div>
-    )
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>MAP</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 50 });
+        }}
+      >
+        Set Position
+      </button>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
